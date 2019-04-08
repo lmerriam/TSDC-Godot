5616a98b1f65ccbbc8473ac4c5c5cb58056e14c1
Logan <loganmerriam@gmail.com>
2019-04-07 17:01:30 -0700

    Initial commit

---
+++ Player.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+export var speed = 5
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var velocity = Vector2()
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	
+	move_and_collide(velocity)
---
+++ TileMap.gd
@@ -0,0 +1,38 @@
+extends TileMap
+
+export var octaves = 4
+export var period = 20.0
+export var persistence = 0.8
+export var grass_threshold = 0.20
+export var water_threshold = 0.00
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	generate_map(octaves,period,persistence,grass_threshold,water_threshold)
+
+func _on_BtnGenMap_button_up():
+	generate_map(octaves,period,persistence,grass_threshold,water_threshold)
+	
+func generate_map(octaves,period,persistence,grass_threshold,water_threshold):
+	clear()
+	
+	var noise = OpenSimplexNoise.new()
+	
+	noise.seed = randi()
+	noise.octaves = octaves
+	noise.period = period
+	noise.persistence = persistence
+	
+	for x in range(200):
+		for y in range(200):
+			var tile
+			var value = noise.get_noise_2d(x,y)
+			if value > grass_threshold:
+				tile = tile_set.find_tile_by_name("Grass")
+			elif value < water_threshold:
+				tile = tile_set.find_tile_by_name("Water")
+			
+			if tile != null:
+				set_cell(x,y,tile)
+	
+	update_bitmask_region(Vector2(0,100),Vector2(0,100))
