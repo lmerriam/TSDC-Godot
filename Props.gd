02ac87f814dfd54de579f90d64638db5a122ddd2
Logan <loganmerriam@gmail.com>
2019-04-10 19:20:26 -0700

    Super random rock props

--- Props.gd
+++ Props.gd
@@ -6,19 +6,18 @@ extends TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-#	generate_props()
+	generate_map()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func generate_props():
-	var noise = OpenSimplexNoise.new()
-	var tile = tile_set.find_tile_by_name("flowers_white")
-	
-	for x in range(200):
-		for y in range(200):
-			
-			if noise.get_noise_2d(x,y) > .3:
-				set_cell(x,y,tile)
+func generate_map():
+	var tiles = ["stump","rock","rock_2"]
+	var tile_name = tiles[rand_range(0,2)]
+	var tile = tile_set.find_tile_by_name(tile_name)
+	for i in 1000:
+		var x = rand_range(0,300)
+		var y = rand_range(0,300)
+		
+		set_cell(x,y,tile)
