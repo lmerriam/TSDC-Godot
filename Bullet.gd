048d6e09e681b10cd4e64ca42267102ed60a9a1e
Logan <loganmerriam@gmail.com>
2019-04-13 12:10:02 -0700

    Bullet firing

--- Bullet.gd
+++ Bullet.gd
@@ -1,13 +1,12 @@
-extends Node2D
+extends Area2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var speed = 300
+var angle = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	global_position += Vector2(-(cos(angle) * speed * delta), -(sin(angle) * speed * delta))	
---
+++ Weapon.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var bullet = preload("res://Bullet.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		var b = bullet.instance()
+		var mouse_pos = get_global_mouse_position()
+		var angle = global_position.angle_to_point(mouse_pos)
+		b.angle = angle
+		b.global_position = global_position
+		get_node("/root/Game/World").add_child(b)
