189262379c967586caa834c4e3b9c4a125cb5b14
Logan <loganmerriam@gmail.com>
2019-06-22 15:43:32 -0700

    Removed extraneous component references from playerinventory

--- PlayerInventory.gd
+++ PlayerInventory.gd
@@ -1,13 +1,11 @@
 extends Control
 
-var inventory
+var player
 var selected_item
-var equipment
 
 func _ready():
-	inventory = Global.player
-	equipment = Global.player
-	Global.player.set_list_control($ItemList)
+	player = Global.player
+	player.set_list_control($ItemList)
 
 func _set_selected_item(item):
 	selected_item = item
@@ -20,24 +18,23 @@ func _set_selected_item(item):
 func _on_ItemList_item_selected(index):
 	var item = $ItemList.get_item_metadata(index)
 	_set_selected_item(item)
-	if equipment.accepts_type(item.type):
+	if player.accepts_type(item.type):
 		$ItemSelected/EquipButton.visible = true
 	else:
 		$ItemSelected/EquipButton.visible = false
 
 func _on_EquipButton_button_up():
-	
 	# Get previous item
 	var item_type = selected_item.get_type()
-	var prev_item = equipment.get_equipped(item_type)
+	var prev_item = player.get_equipped(item_type)
 	
 	# Remove previous item from equipment and send to inv
-	equipment.remove_equipped(prev_item)
-	inventory.add_item(prev_item)
+	player.remove_equipped(prev_item)
+	player.add_item(prev_item)
 	
 	# Remove selected item from inventory and equip
-	inventory.remove_item(selected_item)
-	equipment.set_equipped(selected_item)
+	player.remove_item(selected_item)
+	player.set_equipped(selected_item)
 	
 	# Reset the selected item
 	_set_selected_item(null)
