545b6cff5ff74968ebfce580797b6f0108b3c0ca
Logan <loganmerriam@gmail.com>
2020-01-29 08:19:04 -0800

    Fixed shadowed variables in map generator

--- World.gd
+++ World.gd
@@ -19,19 +19,35 @@ var nodes = []
 var node_edges = []
 var time_before
 
+var tiles = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #	generate_map(noise,map_width,map_height)
 	_gen_nodes()
 	$Environment/Background.rect_size = Vector2(map_width*$Environment.cell_size.x,map_height*$Environment.cell_size.y)
 #	_place_player()
-	_on_chunk_changed(_get_current_chunk(),Vector2(0,0))
 	for node in nodes:
 		var stronghold = load("res://Stronghold.tscn").instance()
 		Global.entities.add_child(stronghold)
 		stronghold.global_position = node.origin*$Environment.cell_size.x
 		node.stronghold = stronghold
 		stronghold.level = node.level * 10
+		
+	for x in map_width:
+		tiles[x] = {}
+		for y in map_height:
+			var value = noise.get_noise_2d(x,y)
+			var tile
+			var grass = $Environment.tile_set.find_tile_by_name("Grass")
+			var water = $Environment.tile_set.find_tile_by_name("Water")
+			if value > grass_threshold:
+				tile = grass
+			elif value < water_threshold:
+				tile = water
+			tiles[x][y] = tile
+	
+	_on_chunk_changed(_get_current_chunk(),Vector2(0,0))
 
 func _process(delta):
 	var chunk = _get_current_chunk()
@@ -57,61 +73,28 @@ func _on_chunk_changed(new_chunk, old_chunk):
 
 	$Environment.clear()
 #	time_before = OS.get_ticks_msec()
-	var start_x = new_chunk.x * chunk_size - chunk_size
-	var end_x = start_x + chunk_size * 3
-	var start_y = new_chunk.y * chunk_size - chunk_size*.75
-	var end_y = start_y + chunk_size * 2.5
-	var grass = $Environment.tile_set.find_tile_by_name("Grass")
-	var water = $Environment.tile_set.find_tile_by_name("Water")
+	var start_x = clamp(new_chunk.x * chunk_size - chunk_size, 0,map_width)
+	var end_x = clamp(start_x + chunk_size * 3, 0, map_width)
+	var start_y = clamp(new_chunk.y * chunk_size - chunk_size*.75, 0, map_height)
+	var end_y = clamp(start_y + chunk_size * 2.5, 0, map_height)
 	for x in range(start_x, end_x):
 		for y in range(start_y, end_y):
-			var tile
-			var value = noise.get_noise_2d(x,y)
-			if value > grass_threshold:
-				tile = grass
-			elif value < water_threshold:
-				tile = water
-
+			var tile = tiles[x][y]
 			if tile != null:
 				$Environment.set_cell(x,y,tile)
 	$Environment.update_bitmask_region(Vector2(start_x,start_y),Vector2(end_x,end_y))
-	
 #	var newtime = OS.get_ticks_msec()
 #	print("Time to set and bitmask tiles: " + String(newtime-time_before))
 
-func set_tile_region(start,end):
-	var start_x = start.x
-	var end_x = end.x
-	var start_y = start.y
-	var end_y = end.y
-	var grass = $Environment.tile_set.find_tile_by_name("Grass")
-	var water = $Environment.tile_set.find_tile_by_name("Water")
-	for x in range(start_x, end_x):
-		for y in range(start_y, end_y):
-			var tile
-			var value = noise.get_noise_2d(x,y)
-			if value > grass_threshold:
-				tile = grass
-			elif value < water_threshold:
-				tile = water
-	$Environment.update_bitmask_region(Vector2(start_x,start_y),Vector2(end_x,end_y))
-
-#func _draw():
-#	for node in nodes:
-#		var cell_size = $Environment.cell_size
-#		draw_circle(node.origin*cell_size.x,32,Color.red)
-#		for neighbor in node.neighbors:
-#			draw_line(node.origin*cell_size,neighbor.origin*cell_size,Color.red)
-
 # Generate base terrain
-func generate_map(noise,width,height):
+func generate_map(_noise,width,height):
 	$Environment.clear()
 
 	time_before = OS.get_ticks_msec()
 	for x in range(width):
 		for y in range(height):
 			var tile
-			var value = noise.get_noise_2d(x,y)
+			var value = _noise.get_noise_2d(x,y)
 			if value > grass_threshold:
 				tile = $Environment.tile_set.find_tile_by_name("Grass")
 			if value < water_threshold:
@@ -132,21 +115,18 @@ func generate_map(noise,width,height):
 func _on_BtnGenMap_button_up():
 	noise = _gen_noise()
 	_on_chunk_changed(current_chunk,current_chunk)
-#	generate_map(noise,map_width,map_height)
-#	_gen_nodes()
-#	_place_player()
 
 func _gen_noise():
 	randomize()
-	var noise = OpenSimplexNoise.new()
+	var _noise = OpenSimplexNoise.new()
 
-	noise.seed = randi()
-	noise.lacunarity = lacunarity
-	noise.octaves = octaves
-	noise.period = period
-	noise.persistence = persistence
+	_noise.seed = randi()
+	_noise.lacunarity = lacunarity
+	_noise.octaves = octaves
+	_noise.period = period
+	_noise.persistence = persistence
 
-	return noise
+	return _noise
 
 func _gen_nodes():
 	nodes.clear()
@@ -186,11 +166,6 @@ func _connect_nodes():
 		unconnected_nodes.erase(node_new)
 		node_new.level = node_existing.level + 1
 #		print("Node new: " + String(node_new.level) + " - Node Existing: " + String(node_existing.level))
-
-#func _level_nodes():
-#	var node = nodes[0]
-#	var node_levels = {}
-	
 
 func _place_player():
 	var origin = nodes[0].origin
