d15435d4d7499058c11a836f6bbe3b0b3d5ce8f0
Logan <loganmerriam@gmail.com>
2020-04-26 17:54:59 -0700

    Fixed sprayweapon/wand to work with latest weapon state stuff

--- items/weapon/WeaponEmitProjectileState.gd
+++ items/weapon/WeaponEmitProjectileState.gd
@@ -1,10 +1,13 @@
 extends State
 
 export var attack_area = preload("res://attacks/Slash.tscn")
+export var angle_vary := 0.0
 
 func enter():
 	var area = attack_area.instance()
-	area.set_angle(owner.aim_force)
+	
+	var angle = owner.aim_force.rotated(rand_range(-angle_vary,angle_vary))
+	area.set_angle(angle)
 	area.set_attack_properties(owner.item_owner.create_attack())
 	owner.add_child(area)
 	area.global_position = owner.global_position
