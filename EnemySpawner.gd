0d69dc47b4564f2a711ef77bcc7090391ac69624
Logan <loganmerriam@gmail.com>
2020-07-24 23:32:28 -0700

    Added spawn rate and max enemy count

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -2,45 +2,62 @@ extends Node2D
 
 export var active := false
 export var max_active_spawns := 5
-export var spawn_rate := 2
+export var spawn_rate := 1.0
 
-var mobs
-var current_mob
+onready var mobs = $SpawnerMobList.get_children()
+var current_mob = 0
+var current_spawn = 0
+var active_spawns : Array
+var spawned_count : int
 var spawn_timer
 
-var locations
-
-var active_spawns : Array
+onready var locations = $SpawnerLocationList.get_children()
+onready var timer := $SpawnTimer
 
 signal mob_defeated
 signal all_mobs_defeated
+signal all_enemies_spawned
 
 func _ready():
-	mobs = $MobList.get_children()
-	locations = $LocationList.get_children()
-	assert(locations.size() > 0) #Need at least one spawn location
-	current_mob = 0
+	assert(locations.size() > 0) # Need at least one spawn location
 	start()
 
 func spawn_mob(mob):
 	for i in mob.count:
 		var enemy = mob.enemy.instance()
-		active_spawns.append(enemy)
-		Global.entities.call_deferred("add_child", enemy)
-		enemy.global_position = locations[randi() % locations.size()].global_position
-		enemy.connect("killed", self, "_on_enemy_killed")
-		enemy.chase_timer = 9999
+
+func spawn_next():
+	if mobs.size() > current_mob:
+		var mob: EnemySpawnerMob = mobs[current_mob]
+		var spawn = mob.instance_spawn()
+		active_spawns.append(spawn)
+		Global.entities.call_deferred("add_child", spawn)
+		spawn.global_position = locations[randi() % locations.size()].global_position
+		spawn.connect("killed", self, "_on_enemy_killed")
+		spawn.chase_timer = 9999 #TODO: this could be better
+		current_spawn += 1
+		spawned_count += 1
+		$SpawnTimer.start(spawn_rate)
+		if current_spawn >= mob.count:
+			current_spawn = 0
+			if current_mob <= mobs.size() - 1:
+				current_mob += 1
+			else:
+				emit_signal("all_enemies_spawned")
+
+func start():
+	call_deferred("spawn_next")
+
+func attempt_to_spawn():
+	if $SpawnTimer.time_left <= 0 and active_spawns.size() < max_active_spawns:
+		spawn_next()
 
 func _on_enemy_killed(enemy):
 	active_spawns.erase(enemy)
 	if active_spawns.size() <= 0:
-		if current_mob < mobs.size()-1:
-			emit_signal("mob_defeated", current_mob)
-			current_mob += 1
-			spawn_mob(mobs[current_mob])
-		else:
+		if current_mob >= mobs.size()-1:
 			emit_signal("all_mobs_defeated")
+	attempt_to_spawn()
 
-func start():
-	call_deferred("spawn_mob", mobs[0])
-
+func _on_timer_up():
+	attempt_to_spawn()
--- EnemySpawnerMob.gd
+++ EnemySpawnerMob.gd
@@ -3,4 +3,11 @@ class_name EnemySpawnerMob
 
 export(PackedScene) var enemy = preload("res://enemies/Enemy.tscn")
 export var count := 1
+
+export var spawn_delay := 0.0
+export var max_spawns := 999
+
 var remaining = count
+
+func instance_spawn():
+	return enemy.instance()
